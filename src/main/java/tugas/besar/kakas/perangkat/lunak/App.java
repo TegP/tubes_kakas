b7885044210a2bc2e16dbeb3b149f70606762df9
Teguh Permana <teguh.permana@outlook.com>
2019-01-04 22:03:45 +0700

    Initial Commit

---
+++ src/main/java/tugas/besar/kakas/perangkat/lunak/App.java
@@ -0,0 +1,13 @@
+package tugas.besar.kakas.perangkat.lunak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/tugas/besar/kakas/perangkat/lunak/Math.java
@@ -0,0 +1,12 @@
+package tugas.besar.kakas.perangkat.lunak;
+
+public class Math {
+	
+	public int mod (int n1, int n2) {
+		return n1%n2;
+	}
+	
+	public static double sqrt (double n) {
+		return Math.sqrt(n);
+	}
+}
